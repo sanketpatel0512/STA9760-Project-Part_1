93ddafa1f3024d6c7455ed4155a5eb80ecd4c214
Sanket Patel <sanketpatel0512@gmail.com>
2020-03-01 18:29:41 -0500

    Added functionality to return complete dataset if num_pages input not provided

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main(page_size,num_pages,output):
 	client = Socrata("data.cityofnewyork.us",APP_KEY) #APP_KEY is APP_TOKEN from Socrata
 	offset = 0
 
+	# If statement to define number of calls if input not provided by user
+	if num_pages == False:
+		lines = client.get("nc67-uf89",select='COUNT(*)')
+		int_count = int(lines[0]['COUNT'])
+		num_pages = (int_count//page_size)+1
+
 	# Open Output file provided by user		
 	if output != False:
 		outfile = open(output, 'w')
@@ -24,7 +30,7 @@ def main(page_size,num_pages,output):
 				json.dump(i, outfile)
 				outfile.write('\n')
 		off_set += page_size
-		print(d)
+		
 
 if __name__ == '__main__':
 	p = 10
